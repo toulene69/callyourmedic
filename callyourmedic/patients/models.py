df19677ad5572609aef6d93aa2b989b2f6e3f1fc
Apoorv <toulene69@gmail.com>
2016-02-03 23:19:31 +0530

    changes to patient model and patient auth model

--- callyourmedic/patients/models.py
+++ callyourmedic/patients/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import auth
 
 from utils import app_utils
 from addresses.models import Address
@@ -8,13 +9,20 @@ class Patients(models.Model):
 
     patient_id          = models.AutoField(primary_key = True)
     patient_org         = models.ForeignKey(Organisation)
-    patient_first_name  = models.CharField(max_length = 100)
-    patient_last_name   = models.CharField(max_length = 100)
-    patient_dob         = models.DateTimeField()
-    patient_date_joined = models.DateTimeField(app_utils.date_default())
-    patient_date_left   = models.DateTimeField()
-    patient_gender      = models.CharField(max_length = 1, choices = app_utils.choices_gender())
-    patient_email       = models.EmailField()
-    patient_phone1      = models.PositiveIntegerField()
-    patient_phone2      = models.PositiveIntegerField(null = True)
-    patient_address     = models.ForeignKey(Address)
+    patient_first_name  = models.CharField(max_length = 100,blank = True)
+    patient_last_name   = models.CharField(max_length = 100, blank = True)
+    patient_dob         = models.DateField(null = True)
+    patient_date_joined = models.DateTimeField(default = app_utils.date_default())
+    patient_date_left   = models.DateTimeField(null = True)
+    patient_gender      = models.CharField(max_length = 1, choices = app_utils.choices_gender(),blank = True)
+    patient_email       = models.EmailField(null = False, blank = False)
+    patient_phone1      = models.CharField(max_length = 15, null = False, blank = False)
+    patient_password    = models.CharField(max_length = 130, null = False, blank = False)
+    patient_address     = models.ForeignKey(Address,blank=True,null=True,)
+
+    def __str__(self): # __unicode__ on Python 2
+        return self.patient_email
+
+class PatientAuthToken(models.Model):
+    patient          = models.ForeignKey(Patients, unique = True)
+    patient_token    = models.CharField(max_length = 64, blank = True, null = True)
